70d86cced81868b1f21aee4c6cbd4e23259ab64a
Jamilson Gomes Moreira Junior <jamilsonjr22@gmail.com>
2017-08-31 21:38:40 -0300

    Componente Previsão criado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrevisaoComponent } from './previsao/previsao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrevisaoComponent
   ],
   imports: [
     BrowserModule
